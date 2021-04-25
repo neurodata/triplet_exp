e7901b35aa3268ed9cb00d915d5bef1259cbfc54
jdey4 <jdey4@jhmi.edu>
2021-04-25 01:48:07 -0400

    new plot

--- benchmarks/experiment_kalaeb_model.py
+++ benchmarks/experiment_kalaeb_model.py
@@ -25,22 +25,26 @@ sample_list = []
 #depth_list = []
 #depths = [3,19]
 #widths = [3,20]
-train_xor_X, train_xor_y, test_xor_X, test_xor_y = get_dataset(N=1000, cov_scale=.25)
-net_xor1 = get_model(n_hidden = 4, hidden_size = 4, penultimate=False, bn=False) 
-net_xor2 = get_model(n_hidden = 17, hidden_size=17, penultimate=False, bn=False)
-l1 = train_model(
+train_xor_X, train_xor_y, test_xor_X, test_xor_y = get_dataset(N=1000, cov_scale=1)
+net_xor1 = get_model(n_hidden = 3, hidden_size = 3, penultimate=False, bn=False) 
+net_xor2 = get_model(n_hidden = 19, hidden_size=20, penultimate=False, bn=False)
+'''l1 = train_model(
                 net_xor1, 
                 train_xor_X, 
                 train_xor_y,
-                iteration=5000
+                iteration=1000
                 )
 l2 = train_model(
                 net_xor2, 
                 train_xor_X, 
                 train_xor_y,
-                iteration=5000
-                )
+                iteration=1000
+                )'''
+model_state_dic = torch.load('saved_model/model_weights_xor_depth_3_.pth',map_location=torch.device('cpu'))
+net_xor1.load_state_dict(model_state_dic)
 
+model_state_dic = torch.load('saved_model/model_weights_xor_depth_19_.pth',map_location=torch.device('cpu'))
+net_xor2.load_state_dict(model_state_dic)
 #%%
 for sample in sample_size:
     for ii in range(reps):
@@ -52,7 +56,7 @@ for sample in sample_size:
         train_rxor_X, train_rxor_y, test_rxor_X, test_rxor_y = get_dataset(
                 N=sample, 
                 angle_param=angle,
-                cov_scale=.25
+                cov_scale=1
                 )
 
         net_xor1_.freeze_net()
@@ -88,5 +92,5 @@ summary['error rxor on xor transformer 1'] = err_rxor_on_xor1
 summary['error rxor on xor transformer 2'] = err_rxor_on_xor2
 summary['rep'] = rep_list
 summary['sample'] = sample_list
-summary.to_csv('triplet_'+str(200)+'_'+str(len(sample_size))+'_'+str(angle)+'.csv')
+summary.to_csv('triplet_cov_scale1_'+str(200)+'_'+str(len(sample_size))+'_'+str(angle)+'.csv')
 # %%
